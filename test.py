b7450d5ac04f3e1bdf53c289e102a1445cac8244
acotanor <acotanor@gmail.com>
2026-03-25 14:14:50 +0100

    Separador de datasets para utilizar datasets viejos. (Aitziber los da separados en el examen)

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def loadModel(model_output: str) -> obj:
             return pickle.load(file)
     except Exception as e:
         print(f"Error al cargar el modelo: {e}")
-        sys.exit(0)
+        sys.exit(1)
 
     
 if __name__ == '__main__':
@@ -32,10 +32,20 @@ if __name__ == '__main__':
 
     config = loadConfig(args.config,"test")
 
-    # Separamos el dataset y obtenemos el 
-
+    # Separamos el dataset
+    data = load_data(config["test_output"]) # Cargamos el dataset del test completo
+    y_true = data[config["column"]].values # Cargamos los valores a predecir
+    data.drop(columns=[config["column"]]) # Separamos los valores a predecir del dataset para poder hacer predicciones.
 
     # Evaluamos cada modelo
     for modelo in config["modelos"]:
         # Cargamos el modelo
+        print(f"Cargando el modelo {modelo}...")
         model = loadModel(config['modelo'])
+        try:
+            test(model, y_true)
+            print(f"Test del modelo {modelo} realizado con éxito.")
+            sys.exit(0)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
